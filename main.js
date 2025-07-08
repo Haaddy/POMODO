032e301337671416fabcb7859aeef15d71e599ff
Haaddy <H00DHaaddy@gmail.com>
2025-07-08 11:38:28 +0300

    refactory in class

--- main.js
+++ main.js
@@ -9,169 +9,88 @@ let pomodoroBtn = document.getElementById('pomodoro');
 let shortBreakBtn = document.getElementById('short');
 let longBreakBtn = document.getElementById('long');
 
-let isStart = false;
 
-let pomodoro = {
-    leftTime: 25 * 60,
-    choise: true,
 
-    UpdateTimer: function () {
-        if (this.leftTime < 0) {
+class Timer {
+    constructor(minutes) {
+         this.initialTime = minutes * 60;
+         this.leftTime = this.initialTime;
+         this.isStart = false;
+    }
+
+     
+
+    UpdateTimer() {
+        if (this.leftTime <= 0) {
             clearInterval(timerID);
             console.log("time end");
             return;
-        }
-
-        let minutes = Math.floor(this.leftTime / 60);
-        let seconds = this.leftTime % 60;
-        const formatTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-
-        console.log(this.leftTime);
-        UItime.innerText = formatTime;
-
+        } 
         this.leftTime--;
-    },
-    resetTime: function(){
-        clearInterval(timerID);
-        this.leftTime = 25*60;
-        let minutes = Math.floor(this.leftTime / 60);
-        let seconds = this.leftTime % 60;
-        const formatTime = `${String(minutes).padStart(2,0)}:${String(seconds).padStart(2,0)}`;
-        UItime.innerText = formatTime;
+        this.updateScreen();
+    }
+
+    resetTime(){
+        this.pause();
+        this.leftTime = this.initialTime;
+        this.updateScreen();
         startBtn.innerText = "start";
     }
-};
-let shortBreak ={
-    leftTime: 5 * 60,
-    choise: false,
 
-    UpdateTimer: function () {
-        if (this.leftTime < 0) {
-            clearInterval(timerID);
-            console.log("time end");
-            return;
-        }
+    setDuration(minutes){
+        this.initialTime = minutes * 60;
+        this.leftTime = this.initialTime;
+        this.updateScreen();
+    }
 
+    updateScreen(){
         let minutes = Math.floor(this.leftTime / 60);
         let seconds = this.leftTime % 60;
         const formatTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 
         console.log(this.leftTime);
         UItime.innerText = formatTime;
+    }
 
-        this.leftTime--;
-    },
-    resetTime: function(){
-        clearInterval(timerID);
-        this.leftTime = 5*60;
-        let minutes = Math.floor(this.leftTime / 60);
-        let seconds = this.leftTime % 60;
-        const formatTime = `${String(minutes).padStart(2,0)}:${String(seconds).padStart(2,0)}`;
-        UItime.innerText = formatTime;
-        startBtn.innerText = "start";
+    pause(){
+        pomodoTimer.isStart = false;
+        clearInterval(timerID)
     }
 }
-let longBreak ={
-    leftTime: 10 * 60,
-    choise: false,
 
-    UpdateTimer: function () {
-        if (this.leftTime < 0) {
-            clearInterval(timerID);
-            console.log("time end");
-            return;
-        }
 
-        let minutes = Math.floor(this.leftTime / 60);
-        let seconds = this.leftTime % 60;
-        const formatTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+const pomodoTimer = new Timer(25);
 
-        console.log(this.leftTime);
-        UItime.innerText = formatTime;
-
-        this.leftTime--;
-    },
-    resetTime: function(){
-        clearInterval(timerID);
-        this.leftTime = 10*60;
-        let minutes = Math.floor(this.leftTime / 60);
-        let seconds = this.leftTime % 60;
-        const formatTime = `${String(minutes).padStart(2,0)}:${String(seconds).padStart(2,0)}`;
-        UItime.innerText = formatTime;
-        startBtn.innerText = "start";
-    }
-}
 
 startBtn.addEventListener("click", () => {
-    if (isStart) {
-        return
-    }
-    isStart = true;
-    if(pomodoro.choise == true){
-        pomodoro.UpdateTimer();
-        startBtn.innerText = "Resume";
-
-        timerID = setInterval(pomodoro.UpdateTimer.bind(pomodoro), 1000);
-    }else if(shortBreak.choise == true){
-        shortBreak.UpdateTimer();
-        startBtn.innerText = "Resume";
-
-        timerID = setInterval(shortBreak.UpdateTimer.bind(shortBreak), 1000);
-    }else if(longBreak.choise == true){
-        longBreak.UpdateTimer();
-        startBtn.innerText = "Resume";
-
-        timerID = setInterval(longBreak.UpdateTimer.bind(longBreak), 1000);
-    }
-    
+    if (pomodoTimer.isStart) return;
+    pomodoTimer.isStart = true;
+    timerID = setInterval(pomodoTimer.UpdateTimer.bind(pomodoTimer), 1000);  
 });
 
 pauseBtn.addEventListener("click", () =>{
-    // pauseTime();
-    isStart = false;
-    clearInterval(timerID);
+    pomodoTimer.pause();
+    
 })
 
 resetBtn.addEventListener("click",()=>{
-    isStart =false;
-    if (pomodoro.choise==true) {
-        pomodoro.resetTime();
-    }else if (shortBreak.choise == true) {
-        shortBreak.resetTime();
-    }else if (longBreak.choise == true) {
-        longBreak.resetTime();
-    }
-   
+    pomodoTimer.resetTime();
 
 })
 
 shortBreakBtn.addEventListener("click", ()=>{
-    isStart= false;
     console.log("short choise");
-    UItime.innerText = "5:00"
-    shortBreak.choise = true;
-    pomodoro.choise = false;
-    longBreak.choise = false;
-    shortBreak.resetTime();
-
+     pomodoTimer.isStart = false;
+    pomodoTimer.setDuration(5);
 })
-
-pomodoroBtn.addEventListener("click", ()=>{
-    isStart= false;
-    console.log("pomodor choise");
-    UItime.innerText = "25:00"
-    shortBreak.choise = false;
-    pomodoro.choise = true;
-    longBreak.choise = false;
-    pomodoro.resetTime();
-})
-
 longBreakBtn.addEventListener("click", ()=>{
-    isStart= false;
-    console.log("long choise");
-    UItime.innerText = "10:00"
-    shortBreak.choise = false;
-    pomodoro.choise = false;
-    longBreak.choise = true;
-    longBreak.resetTime();
+    console.log("short choise");
+     pomodoTimer.isStart = false;
+    pomodoTimer.setDuration(10);
 })
+pomodoroBtn.addEventListener("click", ()=>{
+    console.log("short choise");
+    pomodoTimer.isStart = false;
+    pomodoTimer.setDuration(25);
+})
+
